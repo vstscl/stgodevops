4d5719aae788c5b3e77abcea01c0d773c4d4eab0
vsts cl <vstscl@outlook.com>
2016-09-28 18:39:26 +0000

    Primer Commit

---
+++ StgoDevOps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StgoDevOps.Startup))]
+namespace StgoDevOps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
